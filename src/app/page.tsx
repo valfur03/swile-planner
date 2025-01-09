3070cf9a04727ff07501b72e9d8051809fe0247a
valfur03 <valentin.furmanek@gmail.com>
2025-01-09 10:43:20 +0100

    feat: style the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,13 @@ export default async function Home() {
   const graphData = await buildPlannedPaymentsGraphData(operations.items);
 
   return (
-    <main>
-      {graphData !== null ? <Chart data={graphData} /> : <EmptyChart />}
-    </main>
+    <>
+      <header className="text-center w-full my-6 px-4 flex justify-center">
+        <h1 className="max-w-xl">Qu&apos;ai-je utilisé sur ma Swile ?</h1>
+      </header>
+      <main className="text-center w-full px-4 flex justify-center">
+        {graphData !== null ? <Chart data={graphData} /> : <EmptyChart />}
+      </main>
+    </>
   );
 }
--- src/components/Chart/EmptyChart.tsx
+++ src/components/Chart/EmptyChart.tsx
@@ -6,7 +6,7 @@ export const EmptyChart = ({
   children = <p>No data found.</p>,
 }: EmptyChartProps) => {
   return (
-    <div className="w-full max-w-[672px] md:h-[378px] h-[212px] bg-zinc-100 rounded-xl flex items-center justify-center border-dashed border-2 text-zinc-700">
+    <div className="w-full max-w-2xl md:h-[378px] h-[212px] bg-zinc-100 rounded-xl flex items-center justify-center border-dashed border-2 text-zinc-700">
       {children}
     </div>
   );
