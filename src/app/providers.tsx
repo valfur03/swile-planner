8ca4345f5d7f58d5fc52825ad39461d0ba4a51c9
valfur03 <valentin.furmanek@gmail.com>
2025-01-10 10:11:39 +0100

    feat: initialize Providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { PropsWithChildren } from "react";
+
+export type ProvidersProps = PropsWithChildren;
+
+export const Providers = ({ children }: ProvidersProps) => {
+  return <>{children}</>;
+};
