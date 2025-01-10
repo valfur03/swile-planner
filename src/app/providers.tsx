6f04218465200b117f94dfc6b0cf1b4e32fda24d
valfur03 <valentin.furmanek@gmail.com>
2025-01-10 10:15:43 +0100

    feat: initialize Session context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { PropsWithChildren } from "react";
+import { SessionProvider } from "@/contexts/session/provider";
 
 export type ProvidersProps = PropsWithChildren;
 
 export const Providers = ({ children }: ProvidersProps) => {
-  return <>{children}</>;
+  return <SessionProvider value={null}>{children}</SessionProvider>;
 };
---
+++ src/contexts/session/provider.ts
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+export const SessionContext = createContext(null);
+export const SessionProvider = SessionContext.Provider;
